5a540c712c3246bac8887a2274da850691ee172d
Nathan Apple <napple@vtc.vt.edu>
2014-07-24 21:33:40 -0500

    Completed basic port

--- src/main/scala/TilerUser.scala
+++ src/main/scala/TilerUser.scala
@@ -1,5 +1,6 @@
 object Main extends App {
   val tiler = new Tiler()
   //tiler.prepare("/Users/napple/Downloads/not-amused.png", 0x0000ff)
-  tiler.prepare("/Users/napple/Downloads/finn+shep.jpg")
+  val prepped = tiler.prepare("/Users/napple/Downloads/finn+shep.jpg")
+  tiler.subdivide(prepped, 0, (0, 0), (512, 512))
 }
